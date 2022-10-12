05fbbd6d06ddec8941ce2667622b6ad1d7ff3476
DuyVo - 19Tin_TT <duyvo1945@gmail.com>
2022-10-12 18:52:35 +0700

    updata

---
+++ CheAnh_RAW.py
@@ -0,0 +1,116 @@
+
+import os
+import pathlib
+import cv2
+import numpy as np
+
+
+def path():
+    return pathlib.Path().resolve()
+
+
+# LOAD 1: vuong; LOAD 2 tron
+def CheAnh(x, y, size, path_img, LOAD, output):
+    # doc anh
+    image_r = cv2.imread(path_img)
+    # dpc kich thuoc anh
+    h_max, w_max, c = image_r.shape
+    # lay ten anh and them ban quyen
+    img_name = "duyvo26_" + os.path.split(path_img)[-1]
+    # print('width:  ', w_max)
+    # print('height: ', h_max)
+    # print('channel:', c)
+    leftTop = (x, y)
+    LeftToRight_TopToBottom = (size, size)
+    R = LeftToRight_TopToBottom[0]
+    x, y = leftTop[0], leftTop[1]
+    w, h = LeftToRight_TopToBottom[0], LeftToRight_TopToBottom[1]
+
+    if x > w_max or y > h_max or w > w_max or h > h_max:
+        print("Loi kich thuoc anh")
+        return False
+
+    # hinh vuong
+    if LOAD == 1:
+        print("Vung che vuong")
+        image = image_r
+        # lay vung ra ra hinh vuong
+        # ROI = image[y:y + h, x:x + w]
+        # blur = cv2.GaussianBlur(ROI, (101, 101), 0)
+        # blur = cv2.medianBlur(ROI, 21)
+        # blur = cv2.blur(ROI, (101,101) )
+        ROI = image[y:y + h, x:x + w]
+        size_IMG = (ROI.shape[1], ROI.shape[0])
+        temp = cv2.resize(ROI, (30, 30), interpolation=cv2.INTER_NEAREST)
+        blur = cv2.resize(temp, size_IMG, interpolation=cv2.INTER_NEAREST)
+        # add lam mo vao anh11
+        image[y:y + h, x:x + w] = blur
+        cv2.imwrite(output + "\\" + "(VUONG)_" + img_name, image)
+        return True
+
+        # cv2.imshow("img", image)
+        # cv2.waitKey()
+
+    # hinh tron
+    if LOAD == 2:
+        ROI = image_r.copy()
+        maskShape = (image_r.shape[0], image_r.shape[1], 1)
+        mask = np.full(maskShape, 0, dtype=np.uint8)
+        cv2.circle(ROI, (int((x + x + w) / 2), int((y + y + h) / 2)), int(R / 2), (255, 255, 255), 0)
+        # lay vung ra ra tron
+        # tempImg = cv2.GaussianBlur(tempImg, (51, 51), 0)
+        # ROI = image[y:y + h, x:x + w]
+        # blur = cv2.GaussianBlur(ROI, (101, 101), 0)
+        # blur = cv2.medianBlur(ROI, 21)
+        # blur = cv2.blur(ROI, (101,101) )
+        size_IMG = (ROI.shape[1], ROI.shape[0])
+        temp = cv2.resize(ROI, (60, 60), interpolation=cv2.INTER_NEAREST)
+        tempImg = cv2.resize(temp, size_IMG, interpolation=cv2.INTER_NEAREST)
+
+        cv2.circle(mask, (int((x + x + w) / 2), int((y + y + h) / 2)), int(R / 2), 255, -1)
+        mask_inv = cv2.bitwise_not(mask)
+        img1_bg = cv2.bitwise_and(image_r, image_r, mask=mask_inv)
+        img2_fg = cv2.bitwise_and(tempImg, tempImg, mask=mask)
+        dst = cv2.add(img1_bg, img2_fg)
+
+        cv2.imwrite(output + "\\" + "(TRON)_" + img_name, dst)
+        return True
+        # cv2.imshow("img", dst)
+        # cv2.waitKey()
+
+
+def GetIMGinFloder(x, y, r, status, folder, output):
+    SumFile = 0  # sum file
+    LOG_TXT = ""
+    for (root, dirs, file) in os.walk(folder):  # lap lay danh sach
+        for f in file:
+            if ".jpg" in f or ".png" in f or ".webp" in f:
+                SumFile += 1
+                FileIMG = root + "\\" + f
+                try:
+                    if CheAnh(x, y, r, FileIMG, status, output):
+                        print("ANH :\t", f, "\t Hoan Thanh")
+                        LOG_TXT += "----------------------\n"
+                        LOG_TXT += "ANH :\t" + f + "\t Hoan Thanh" + "\n"
+                    else:
+                        print("ANH :\t", f, "\t Co loi bo qua")
+                        LOG_TXT += "----------------------\n"
+                        LOG_TXT += "ANH :\t" + f + "\t Co loi bo qua" + "\n"
+                except Exception as mess:
+                    print(mess)
+                    # print("ANH :\t", f, "\t Co loi bo qua")
+                    LOG_TXT += "----------------------\n"
+                    LOG_TXT += "ANH :\t" + f + "\t Co loi bo qua" + "\n"
+    return LOG_TXT
+
+
+def InputData(x, y, r, status, input_path):
+    return GetIMGinFloder(x, y, r, status, input_path, str(path()) + "\\img_out")
+
+# x = 250
+# y = 900
+# r = 600
+# status = 1
+# txt = str(r"D:\USER\Pictures\xy ly anh")
+#
+# InputData(x, y, r, status, txt)
--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# import  CheAnh
-#
-# CheAnh.CheAnh(100, 200, 300,"img.jpg", 2)
+import os
+
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import sys
 
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+# khai bao giao dien
+ui_main, _ = loadUiType('main.ui')
 
-Form, Window = uic.loadUiType("main.ui")
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec()
+class MainApp(QMainWindow, ui_main):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        # click nut bat dau
+        self.btn_batdau_vuong.clicked.connect(self.ThucThi_vuong)
+        self.btn_batdau_tron.clicked.connect(self.ThucThi_tron)
+
+    def ThucThi_vuong(self):
+        path_Folder = str(self.path_folder.text())
+        X = int(self.point_x.text())
+        Y = int(self.point_y.text())
+        R = int(self.size_che.text())
+        txt = str(path_Folder)
+        import CheAnh
+        Log = CheAnh.InputData(X, Y, R, 1, txt)
+        Log = list(Log)
+        # showLoad
+        self.txt_log.insertPlainText(Log[0])
+        QMessageBox.information(self, "Thông báo", "Hoàn thành che anh theo hinh vuông")
+        os.startfile(Log[1])
+
+    def ThucThi_tron(self):
+        path_Folder = str(self.path_folder.text())
+        X = int(self.point_x.text())
+        Y = int(self.point_y.text())
+        R = int(self.size_che.text())
+        txt = str(path_Folder)
+        import CheAnh
+        Log = CheAnh.InputData(X, Y, R, 2, txt)
+        Log = list(Log)
+        # showLoad
+        self.txt_log.insertPlainText(Log[0])
+        QMessageBox.information(self, "Thông báo", "Hoàn thành che ảnh theo hình tròn")
+        os.startfile(Log[1])
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec()
+
+
+# tu tao thu muc
+# and file cmd
