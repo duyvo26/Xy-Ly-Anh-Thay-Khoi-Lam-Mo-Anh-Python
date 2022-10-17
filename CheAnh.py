4b9c5c302ef68c2eb71933fc15ddb85fac961f67
DuyVo - 19Tin_TT <duyvo1945@gmail.com>
2022-10-18 00:20:31 +0700

    upload tinh nagn moi

--- CheAnh.py
+++ CheAnh.py
@@ -9,30 +9,36 @@ def path():
 
 
 # LOAD 1: vuong; LOAD 2 tron
-def CheAnh(x, y, size, path_img, LOAD, output):
+def CheAnh(x, y, size, path_img, LOAD, output, CuongDo):
     image_r = cv2.imread(path_img)
     h_max, w_max, c = image_r.shape
     img_name = "duyvo26_" + os.path.split(path_img)[-1]
+
+
     leftTop = (x, y)
     LeftToRight_TopToBottom = (size, size)
     R = LeftToRight_TopToBottom[0]
     x, y = leftTop[0], leftTop[1]
     w, h = LeftToRight_TopToBottom[0], LeftToRight_TopToBottom[1]
-    if x > w_max or y > h_max or w > w_max or h > h_max:
-        print("Loi kich thuoc anh")
+
+
+    if x > w_max - R or y > h_max - R or w > w_max - R or h > h_max - R:
+        # print("Loi kich thuoc anh")
         return False
 
+# 5 - 40
     # hinh vuong
     if LOAD == 1:
-        print("Vung che vuong")
+        # print("Vung che vuong")
         image = image_r
         ROI = image[y:y + h, x:x + w]
         size_IMG = (ROI.shape[1], ROI.shape[0])
-        temp = cv2.resize(ROI, (30, 30), interpolation=cv2.INTER_NEAREST)
+        CuongDo = int((((h_max - (h_max * CuongDo) / 100) / 10) + 10) / 4)
+        temp = cv2.resize(ROI, (CuongDo, CuongDo), interpolation=cv2.INTER_NEAREST)
         blur = cv2.resize(temp, size_IMG, interpolation=cv2.INTER_NEAREST)
         image[y:y + h, x:x + w] = blur
         cv2.imwrite(output + "\\" + "(VUONG)_" + img_name, image)
-        return True
+        return output + "\\" + "(VUONG)_" + img_name
     # hinh tron
     if LOAD == 2:
         ROI = image_r.copy()
@@ -40,7 +46,8 @@ def CheAnh(x, y, size, path_img, LOAD, output):
         mask = np.full(maskShape, 0, dtype=np.uint8)
         cv2.circle(ROI, (int((x + x + w) / 2), int((y + y + h) / 2)), int(R / 2), (255, 255, 255), 0)
         size_IMG = (ROI.shape[1], ROI.shape[0])
-        temp = cv2.resize(ROI, (60, 60), interpolation=cv2.INTER_NEAREST)
+        CuongDo = int((((h_max - (h_max * CuongDo) / 100) / 10) + 10))
+        temp = cv2.resize(ROI, (CuongDo, CuongDo), interpolation=cv2.INTER_NEAREST)
         tempImg = cv2.resize(temp, size_IMG, interpolation=cv2.INTER_NEAREST)
         cv2.circle(mask, (int((x + x + w) / 2), int((y + y + h) / 2)), int(R / 2), 255, -1)
         mask_inv = cv2.bitwise_not(mask)
@@ -48,10 +55,10 @@ def CheAnh(x, y, size, path_img, LOAD, output):
         img2_fg = cv2.bitwise_and(tempImg, tempImg, mask=mask)
         dst = cv2.add(img1_bg, img2_fg)
         cv2.imwrite(output + "\\" + "(TRON)_" + img_name, dst)
-        return True
+        return output + "\\" + "(TRON)_" + img_name
 
 
-def GetIMGinFloder(x, y, r, status, folder, output):
+def GetIMGinFloder(x, y, r, status, folder, output, CuongDo):
     SumFile = 0  # sum file
     LOG_TXT = ""
     for (root, dirs, file) in os.walk(folder):  # lap lay danh sach
@@ -60,22 +67,40 @@ def GetIMGinFloder(x, y, r, status, folder, output):
                 SumFile += 1
                 FileIMG = root + "\\" + f
                 try:
-                    if CheAnh(x, y, r, FileIMG, status, output):
-                        print("ANH :\t", f, "\t Hoan Thanh")
-                        LOG_TXT += "----------------------\n"
-                        LOG_TXT += str(SumFile) + "\n" + "ANH :\t" + f + "\t Hoan Thanh" + "\n"
-                    else:
-                        print("ANH :\t", f, "\t Co loi bo qua")
+                    if CheAnh(x, y, r, FileIMG, status, output, CuongDo) == False:
+                        # print("ANH :\t", f, "\t Co loi bo qua")
                         LOG_TXT += "----------------------\n"
                         LOG_TXT += str(SumFile) + "\n" + "ANH :\t" + f + "\t Co loi bo qua" + "\n"
                 except Exception as mess:
-                    print("ANH :\t", f, "\t Co loi bo qua")
+                    # print("ANH :\t", f, "\t Co loi bo qua")
                     LOG_TXT += "----------------------\n"
                     LOG_TXT += str(SumFile) + "\n" + "ANH :\t" + f + "\t Co loi bo qua" + "\n"
     return LOG_TXT
 
+def GetIMGinFloderDEmo(x, y, r, status, folder, output, CuongDo, coutIMG):
+    SumFile = 0  # sum file
+    LOG_TXT = ""
+    for (root, dirs, file) in os.walk(folder):  # lap lay danh sach
+        if coutIMG is None:
+            if SumFile > coutIMG:
+                break
+        for f in file:
+            if ".jpg" in f or ".png" in f or ".webp" in f:
+                SumFile += 1
+                FileIMG = root + "\\" + f
+                try:
+                    kq = CheAnh(x, y, r, FileIMG, status, output, CuongDo)
+                    if  kq== False:
+                        return "Co lỗi khi nhập kich thươc ảnh vui lòng thử lại"
+                    else:
+                        return kq
+                except Exception as mess:
+                    return False
 
-def InputData(x, y, r, status, input_path):
-    yield GetIMGinFloder(x, y, r, status, input_path, str(path()) + "\\img_out")
+
+
+
+def InputData(x, y, r, status, input_path, CuongDo):
+    yield GetIMGinFloder(x, y, r, status, input_path, str(path()) + "\\img_out", CuongDo)
     yield str(path()) + "\\img_out"
 
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
-
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
+from PyQt5.QtGui import QPixmap
+
 import sys
 
-
 # khai bao giao dien
+from pyqt5_plugins.examplebuttonplugin import QtGui
+
 ui_main, _ = loadUiType('main.ui')
 
 
@@ -14,8 +18,62 @@ class MainApp(QMainWindow, ui_main):
         QMainWindow.__init__(self)
         self.setupUi(self)
         # click nut bat dau
+        self.point_x.textChanged.connect(self.DemoIMG)
+        self.point_y.textChanged.connect(self.DemoIMG)
+        self.size_che.textChanged.connect(self.DemoIMG)
+        self.thanh_dieuchinh.valueChanged.connect(self.DemoIMG)
+
         self.btn_batdau_vuong.clicked.connect(self.ThucThi_vuong)
         self.btn_batdau_tron.clicked.connect(self.ThucThi_tron)
+        self.thanh_dieuchinh.valueChanged.connect(self.CuongDo)
+        self.SetViewCuongDo(5)
+        # check nhap du lieu
+
+    def DemoIMG(self):
+        path_Folder = str(self.path_folder.text())
+        X = int(self.point_x.text())
+        Y = int(self.point_y.text())
+        R = int(self.size_che.text())
+
+
+        if X > 0 and Y > 0 and R > 0 and path_Folder != "":
+            txt = str(path_Folder)
+            import CheAnh
+            CuongDo = self.thanh_dieuchinh.value()
+            path_Vuong = CheAnh.GetIMGinFloderDEmo(X, Y, R, 1, txt, "img_out", CuongDo, 2)
+            print(path_Vuong)
+            path_Tron = CheAnh.GetIMGinFloderDEmo(X, Y, R, 2, txt, "img_out", CuongDo, 2)
+            if "lỗi" not in path_Vuong:
+                self.ShowImg_Vuong(path_Vuong)
+                self.ShowImg_Tron(path_Tron)
+            else:
+                # showLoad
+                self.txt_log.insertPlainText(path_Tron+"\n")
+                # QMessageBox.information(self, "Thông báo", path_Tron)
+
+
+    def ShowImg_Vuong(self, file_path):
+        label = self.findChild(QLabel, "labviewVuong")
+        pixmap = QPixmap(file_path)
+        pixmap = pixmap.scaled(450, 350)
+        label.setPixmap(pixmap)
+        label.resize(pixmap.width(), pixmap.height())
+
+    def ShowImg_Tron(self, file_path):
+        label = self.findChild(QLabel, "labviewTron")
+        pixmap = QPixmap(file_path)
+        pixmap = pixmap.scaled(450, 350)
+        label.setPixmap(pixmap)
+        label.resize(pixmap.width(), pixmap.height())
+
+    def CuongDo(self):
+        CuongDo = self.thanh_dieuchinh.value()
+        self.SetViewCuongDo(CuongDo)
+
+    def SetViewCuongDo(self, txt):
+        txt = str(txt)
+        self.label = self.findChild(QLabel, "lableCuongDo")
+        self.label.setText(txt + "%")
 
     def ThucThi_vuong(self):
         path_Folder = str(self.path_folder.text())
@@ -24,7 +82,9 @@ class MainApp(QMainWindow, ui_main):
         R = int(self.size_che.text())
         txt = str(path_Folder)
         import CheAnh
-        Log = CheAnh.InputData(X, Y, R, 1, txt)
+        CuongDo = self.thanh_dieuchinh.value()
+        # CuongDo = 45 - CuongDo
+        Log = CheAnh.InputData(X, Y, R, 1, txt, CuongDo)
         Log = list(Log)
         # showLoad
         self.txt_log.insertPlainText(Log[0])
@@ -38,7 +98,8 @@ class MainApp(QMainWindow, ui_main):
         R = int(self.size_che.text())
         txt = str(path_Folder)
         import CheAnh
-        Log = CheAnh.InputData(X, Y, R, 2, txt)
+        CuongDo = self.thanh_dieuchinh.value()
+        Log = CheAnh.InputData(X, Y, R, 2, txt, CuongDo)
         Log = list(Log)
         # showLoad
         self.txt_log.insertPlainText(Log[0])
@@ -51,7 +112,6 @@ if __name__ == "__main__":
     window = MainApp()
     window.show()
     app.exec()
-
 
 # tu tao thu muc
 # and file cmd
